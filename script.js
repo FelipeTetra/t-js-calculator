e1cf04fa21bbffd05bc6951dacb72898c0b2a40c
Felipe Rei <felipedm600@gmail.com>
2025-10-19 02:01:02 -0300

    JS Added - Finished

---
+++ script.js
@@ -0,0 +1,53 @@
+"use strict"
+function search(e, all = false){
+  if (!all) 
+  return document.querySelector(e);
+  else {
+    return Array.from(document.querySelectorAll(e));
+  }
+}
+
+const numbers = search('[data-calc="number"]', true);
+const operators = search('[data-calc="operator"]', true);
+const result = search('[data-calc="="]');;
+const clear = search('[data-calc="clear"]');
+const input = search('input');
+
+numbers.forEach(e => {
+  e.addEventListener('click', t => {
+    input.value += e.innerHTML
+  })
+})
+ 
+result.addEventListener('click', t => {
+  const x = eval(input.value);
+  let parts = x.toString().split(".");
+
+  if (!parts[1].length > 2) {
+    return input.value = x;
+  }
+  input.value = x.toFixed(2);
+})
+
+operators.forEach( e => {
+  e.addEventListener('click', t => {
+    if (
+      e.innerHTML == '-' && input.value[input.value.length-1] != '-' && input.value[input.value.length-1] != '+' ||
+      e.innerHTML == '+' && input.value[input.value.length-1] != '+' && input.value[input.value.length-1] != '-') 
+      {
+      return input.value += e.innerHTML;
+    } 
+    if (
+      input.value[input.value.length-1] !== '/' &&
+      input.value[input.value.length-1] !== '-' &&
+      input.value[input.value.length-1] !== '+' &&
+      input.value[input.value.length-1] !== '*') 
+      {
+      return input.value += e.innerHTML;
+    }
+  })
+})
+
+clear.addEventListener('click', type => {
+  input.value = null;
+})
